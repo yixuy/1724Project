e71fc53cb510b91360a0504ecee21ec2a9c03d8a
Zhaoyu Yan <zhaoyu.yan@mail.utoronto.ca>
2024-12-05 20:55:24 -0500

    add auth messages to display to user

--- client/src/components/signin.rs
+++ client/src/components/signin.rs
@@ -1,5 +1,6 @@
 use crate::router::Route;
 use serde::Serialize;
+use serde_json::Value;
 use stylist::style;
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
@@ -15,52 +16,52 @@ pub fn sign_in() -> Html {
     let navigator = use_navigator().unwrap();
     let css = style!(
         r#"
-    .signin-container {
-    background-color: #4287f5; /* Card's background color */
-    display: flex; /* Enable flexbox */
-    justify-content: center; /* Center horizontally */
-    align-items: center; /* Center vertically */
-    height: 100vh; /* Full height of the viewport */
-    width: 100vw; /* Full width of the viewport */
-    box-sizing: border-box; /* Include padding and border in dimensions */
-    margin: 0; /* Remove any extra margins */
-}
+        .signin-container {
+            background-color: #4287f5; /* Card's background color */
+            display: flex; /* Enable flexbox */
+            justify-content: center; /* Center horizontally */
+            align-items: center; /* Center vertically */
+            height: 100vh; /* Full height of the viewport */
+            width: 100vw; /* Full width of the viewport */
+            box-sizing: border-box; /* Include padding and border in dimensions */
+            margin: 0; /* Remove any extra margins */
+        }
 
-.signin-form {
-    background: white; /* Background color of the form */
-    padding: 20px; /* Padding inside the form */
-    border-radius: 8px; /* Rounded corners */
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Add shadow */
-    text-align: center; /* Center the text */
-    width: 90%; /* Responsive width */
-    max-width: 300px; /* Restrict the form's maximum size */
-}
+        .signin-form {
+            background: white; /* Background color of the form */
+            padding: 20px; /* Padding inside the form */
+            border-radius: 8px; /* Rounded corners */
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Add shadow */
+            text-align: center; /* Center the text */
+            width: 90%; /* Responsive width */
+            max-width: 300px; /* Restrict the form's maximum size */
+        }
 
-    input[type="text"],
-    input[type="password"] {
-        width: 80%;
-        padding: 10px;
-        margin-bottom: 10px;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-    }
+        input[type="text"],
+        input[type="password"] {
+            width: 80%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
 
-    button {
-        width: 70%;
-        padding: 10px;
-        color: white;
-        background-color: #007BFF;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-    }
+        button {
+            width: 70%;
+            padding: 10px;
+            color: white;
+            background-color: #007BFF;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
 
-    button:hover {
-        background-color: #0056b3;
-    }
-    "#
-    )
-    .unwrap();
+        button:hover {
+            background-color: #0056b3;
+        }
+        "#
+    ).unwrap();
+
     let go_home_button = {
         let navigator = navigator.clone();
         let onclick = Callback::from(move |_| navigator.push(&Route::Home));
@@ -80,7 +81,7 @@ pub fn sign_in() -> Html {
     let on_submit = {
         let username = username.clone();
         let password = password.clone();
-        // let message = message.clone();
+        let message = message.clone();
 
         Callback::from(move |e: SubmitEvent| {
             e.prevent_default();
@@ -89,6 +90,7 @@ pub fn sign_in() -> Html {
                 password: (*password).clone(),
             };
             let navigator = navigator.clone();
+            let message = message.clone();
 
             wasm_bindgen_futures::spawn_local(async move {
                 match reqwest::Client::new()
@@ -97,10 +99,15 @@ pub fn sign_in() -> Html {
                     .send()
                     .await
                 {
-                    Ok(response) if response.status().is_success() => {
-                        // message.set("Sign-in successful!".to_string());
-                        navigator.push(&Route::Home);
-                    }
+                    Ok(response) => {
+                        if response.status().is_success() {
+                            navigator.push(&Route::Home);
+                        } else {
+                            let body = response.text().await.unwrap();
+                            let v: Value = serde_json::from_str(&body).unwrap();
+                            message.set(v["message"].as_str().unwrap().to_string());
+                        }
+                    },
                     _ => {
                         web_sys::console::log_1(&"Sign-in failed!".into());
                         // message.set("Sign-in failed!".to_string());
@@ -130,7 +137,7 @@ pub fn sign_in() -> Html {
       <div class={css.get_class_name().to_string()}>
         <div class="signin-container">
             <form onsubmit={Some(on_submit)} class="signin-form">
-            <div >
+            // <div >
                 <h1>{"Sign In"}</h1>
                 <input
                     type="text"
@@ -147,9 +154,9 @@ pub fn sign_in() -> Html {
                 />
                 <br/>
                 <button type="submit">{"Sign In"}</button>
-                </div>
+                <p>{(*message).clone()}</p>
+                // </div>
             </form>
-            // <p>{(*message).clone()}</p>
         </div>
     </div>
     }
--- client/src/components/signup.rs
+++ client/src/components/signup.rs
@@ -11,56 +11,55 @@ struct SignUpData {
 }
 
 #[function_component(SignUp)]
-pub fn sign_in() -> Html {
+pub fn sign_up() -> Html {
     let navigator = use_navigator().unwrap();
     let css = style!(
         r#"
-    .SignUp-container {
-    background-color: #4287f5; /* Card's background color */
-    display: flex; /* Enable flexbox */
-    justify-content: center; /* Center horizontally */
-    align-items: center; /* Center vertically */
-    height: 100vh; /* Full height of the viewport */
-    width: 100vw; /* Full width of the viewport */
-    box-sizing: border-box; /* Include padding and border in dimensions */
-    margin: 0; /* Remove any extra margins */
-}
+        .SignUp-container {
+            background-color: #4287f5; /* Card's background color */
+            display: flex; /* Enable flexbox */
+            justify-content: center; /* Center horizontally */
+            align-items: center; /* Center vertically */
+            height: 100vh; /* Full height of the viewport */
+            width: 100vw; /* Full width of the viewport */
+            box-sizing: border-box; /* Include padding and border in dimensions */
+            margin: 0; /* Remove any extra margins */
+        }
 
-.SignUp-form {
-    background: white; /* Background color of the form */
-    padding: 20px; /* Padding inside the form */
-    border-radius: 8px; /* Rounded corners */
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Add shadow */
-    text-align: center; /* Center the text */
-    width: 90%; /* Responsive width */
-    max-width: 300px; /* Restrict the form's maximum size */
-}
+        .SignUp-form {
+            background: white; /* Background color of the form */
+            padding: 20px; /* Padding inside the form */
+            border-radius: 8px; /* Rounded corners */
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Add shadow */
+            text-align: center; /* Center the text */
+            width: 90%; /* Responsive width */
+            max-width: 300px; /* Restrict the form's maximum size */
+        }
 
-    input[type="text"],
-    input[type="password"] {
-        width: 80%;
-        padding: 10px;
-        margin-bottom: 10px;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-    }
+        input[type="text"], input[type="password"] {
+            width: 80%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
 
-    button {
-        width: 70%;
-        padding: 10px;
-        color: white;
-        background-color: #007BFF;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-    }
+        button {
+            width: 70%;
+            padding: 10px;
+            color: white;
+            background-color: #007BFF;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
 
-    button:hover {
-        background-color: #0056b3;
-    }
-    "#
-    )
-    .unwrap();
+        button:hover {
+            background-color: #0056b3;
+        }
+        "#
+    ).unwrap();
+
     let go_home_button = {
         let navigator = navigator.clone();
         let onclick = Callback::from(move |_| navigator.push(&Route::Home));
@@ -75,12 +74,15 @@ pub fn sign_in() -> Html {
 
     let username = use_state(|| "".to_string());
     let password = use_state(|| "".to_string());
-    // let message = use_state(|| "".to_string());
+    let message = use_state(|| "".to_string());
+    let success = use_state(|| false);
 
     let on_submit = {
         let username = username.clone();
         let password = password.clone();
-        // let message = message.clone();
+        let message = message.clone();
+        let success = success.clone();
+
 
         Callback::from(move |e: SubmitEvent| {
             e.prevent_default();
@@ -88,7 +90,8 @@ pub fn sign_in() -> Html {
                 username: (*username).clone(),
                 password: (*password).clone(),
             };
-            let navigator = navigator.clone();
+            let message = message.clone();
+            let success = success.clone();
 
             wasm_bindgen_futures::spawn_local(async move {
                 match reqwest::Client::new()
@@ -97,12 +100,15 @@ pub fn sign_in() -> Html {
                     .send()
                     .await
                 {
-                    Ok(response) if response.status().is_success() => {
-                        // message.set("Sign-in successful!".to_string());
-                        navigator.push(&Route::SignIn);
-                    }
+                    Ok(response) => {
+                        if response.status().is_success() {
+                            success.set(true);
+                        } else {
+                            message.set(response.text().await.unwrap().to_string());
+                        }
+                    },
                     _ => {
-                        // message.set("Sign-in failed!".to_string());
+                        message.set("Sign-up failed! Please try again.".to_string());
                     }
                 }
             });
@@ -125,31 +131,41 @@ pub fn sign_in() -> Html {
         })
     };
 
+    let on_success = {
+        let navigator = navigator.clone();
+        Callback::from(move |_| navigator.push(&Route::SignIn))
+    };
+
     html! {
-      <div class={css.get_class_name().to_string()}>
+        <div class={css.get_class_name().to_string()}>
         <div class="SignUp-container">
-            <form onsubmit={Some(on_submit)} class="SignUp-form">
-            <div >
-                <h1>{"Sign Up"}</h1>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={(*username).clone()}
-                    oninput={on_username_change}
-                />
-                <br/>
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={(*password).clone()}
-                    oninput={on_password_change}
-                />
-                <br/>
-                <button type="submit">{"Sign Up"}</button>
+            if *success {
+                <div class="SignUp-form">
+                    <h1>{"Sign-up successful!"}</h1>
+                    <button onclick={on_success}>{"Go to Sign-In"}</button>
                 </div>
-            </form>
-            // <p>{(*message).clone()}</p>
+            } else {
+                <form onsubmit={Some(on_submit)} class="SignUp-form">
+                    <h1>{"Sign Up"}</h1>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={(*username).clone()}
+                        oninput={on_username_change}
+                    />
+                    <br/>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={(*password).clone()}
+                        oninput={on_password_change}
+                    />
+                    <br/>
+                    <button type="submit">{"Sign Up"}</button>
+                    <p>{(*message).clone()}</p>
+                </form>
+            }
         </div>
-    </div>
+        </div>
     }
 }
